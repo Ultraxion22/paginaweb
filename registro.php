39cc71923e819bda5f1c10f25d0fe87beee863c4
ultraxion22 <antonio.vargas_a@mail.udp.cl>
2021-06-02 21:42:04 +0000

    first commit

---
+++ registro.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Social UDP</title>
+        <link rel="stylesheet" href="CSS/styles.css">
+        <link rel="icon" href="favicon.ico">
+    </head>
+    <body>
+        <div class="franja">
+            <a href="index.html"><img class="img1" src="images/C95uV6hXUAA3mI7.jpg" alt="logo UDP"></a>
+        </div>
+        <div class="register">
+            <h1 class ="h">Registro</h1>
+            <hr>
+            <form action="foto.html">
+                <input class="input"  type="text">
+                <label>Nombre</label><br>
+                <input class="input" type="date">
+                <label for="">Fecha de nacimiento</label><br>
+                <input class="input" type="text" name="" id="">
+                <label for="">Colegio/Universidad</label><br>
+                <input class="input"  type="email">
+                <label for="">Email</label><br>
+                <input class="input" type="password" name="" id="">
+                <label for="">Password</label><br>
+                <br>
+                <input type="checkbox" name="" id="">
+                <label for="">Al crear una cuenta, aceptas nuestros <a href="images/gatito-m.jpg">Terminos y servicios</a>.</label><br>
+                <br>
+                <button type="submit" class="registerbtn">Registrarse</button>
+    
+                <div class="container signin">
+                    <p>Ya tienes una cuenta? <a href="sign.html">Inicia Sesion</a>.</p>
+                  </div>
+            </form>
+        </div>
+        
+        
+    </body>
+</html>
---
+++ sign.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Social UDP</title>
+        <link rel="stylesheet" href="CSS/styles.css">
+        <link rel="icon" href="favicon.ico">
+    </head>
+    <body>
+        <div class="franja">
+            <a href="index.html"><img class="img1" src="images/C95uV6hXUAA3mI7.jpg" alt="logo UDP"></a>
+        </div>
+        <div class="register">
+            <h1 class ="h">Inicio Sesion</h1>
+            <hr>
+            <form action="checklogin.php" method = "POST">
+                <input class="input" type="email">
+                <label for="">Email</label><br>
+                <input class="input" type="password" name="" id="">
+                <label for="">Password</label><br>
+                <button type="submit" class="registerbtn">Ingresar</button>
+            </form>
+        </div>
+        
+        
+    </body>
+</html>
